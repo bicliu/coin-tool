768ecacd2ff58f24c8255c2f7090187d70346830
Bic Liu <30335141+bicliu@users.noreply.github.com>
2018-05-11 00:48:25 +0800

    add reward count

--- tools/toolMain.cpp
+++ tools/toolMain.cpp
@@ -5,6 +5,56 @@
 
 using namespace std;
 
+void YearsReward(int iyears)
+{
+	cout << endl << "The rewards in every "<< iyears << " from 1st to 4th :" <<endl
+		<< "year    height range        MinerSubsidy           Budget        MasternodePayment     FoundersReward       BlockSubsidy" <<endl;
+	for(int i = 1; i < 5; i++)
+	{
+		CAmount aMiner = 0;
+		CAmount aBud = 0;
+		CAmount aMnode = 0;
+		CAmount aFounders = 0;
+		CAmount aBlocks = 0;
+		int iHl = (i - 1) * 210240 * iyears;
+		int iHh = i * 210240 * iyears;
+		for (int h = iHl; h < iHh; h ++)
+		{
+			aMiner += GetMinerSubsidy(h, Params().GetConsensus());
+			aBud += GetBudget(h, Params().GetConsensus());
+			aMnode += GetMasternodePayment(h);
+			aFounders += GetFoundersReward(h, Params().GetConsensus());
+			aBlocks += (aMiner + aBud + aMnode + aFounders);
+
+			cout << i << " x" << iyears <<setw(9) << iHl << "--" << (iHh - 1)
+				<< setw(20) << aMiner << setw(20) << aBud
+				<< setw(20) << aMnode << setw(20) << aFounders
+				<< setw(20) << aBlocks << endl;
+		}
+	}
+}
+
+void RewardHandle()
+{
+	if (mapArgs.count("-height"))
+	{
+		cout << "  height      MinerSubsidy           Budget        MasternodePayment     FoundersReward       BlockSubsidy" << endl;
+		for(string str : mapMultiArgs["-height"])
+		{
+			int h = atoi(str);
+			cout << setw(10) << h
+				<< setw(20) << GetMinerSubsidy(h, Params().GetConsensus()) / COIN
+				<< setw(20) << GetBudget(h, Params().GetConsensus()) / COIN
+				<< setw(20) << GetMasternodePayment(h) / COIN
+				<< setw(20) << GetFoundersReward(h, Params().GetConsensus()) / COIN
+				<< setw(20) << GetBlockSubsidy(h, Params().GetConsensus()) / COIN << endl;
+		}
+	}
+	
+	YearsReward(1);
+	YearsReward(4);
+}
+
 int main(int argc, char* argv[])
 {
 	SetFilePath("tool.conf");
@@ -21,24 +71,9 @@ int main(int argc, char* argv[])
 	if(!mapArgs.count("-toolcommand"))
 		return showreturn("File without command");
 
-	if("subsidy" == mapArgs["-toolcommand"])
+	if("reward" == mapArgs["-toolcommand"])
 	{
-		if (mapArgs.count("-height"))
-		{
-			cout << "  height      MinerSubsidy           Budget        MasternodePayment     FoundersReward       BlockSubsidy" << endl;
-			for(string str : mapMultiArgs["-height"])
-			{
-				int h = atoi(str);
-				cout << setw(10) << h
-					<< setw(20) << GetMinerSubsidy(h, Params().GetConsensus()) / COIN
-					<< setw(20) << GetBudget(h, Params().GetConsensus()) / COIN
-					<< setw(20) << GetMasternodePayment(h) / COIN
-					<< setw(20) << GetFoundersReward(h, Params().GetConsensus()) / COIN
-					<< setw(20) << GetBlockSubsidy(h, Params().GetConsensus()) / COIN << endl;
-			}
-		}
-		else
-			return showreturn("file content set : height=x (multi)");
+		RewardHandle();
 	}
 
     return 0;
