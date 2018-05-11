79c4bdf6f3cf1d879092a2e9e6354ae81adcab71
bicliu <klins@163.com>
2018-05-11 14:55:40 +0800

    finish reward

--- tools/toolMain.cpp
+++ tools/toolMain.cpp
@@ -1,43 +1,81 @@
 #include "utils.h"
 #include "main.h"
+#include "governance-classes.h"
 
 #include <iomanip>
 
 using namespace std;
 
-void YearsReward(int iyears)
+void AddReward(const CAmount value, CAmount & coin, CAmount & small)
 {
-	cout << endl << "The rewards in every "<< iyears << " from 1st to 4th :" <<endl
-		<< "year    height range        MinerSubsidy           Budget        MasternodePayment     FoundersReward       BlockSubsidy" <<endl;
-	for(int i = 1; i < 5; i++)
+	coin += (value / COIN);
+	small += (value % COIN);
+	if(small > COIN)
 	{
-		CAmount aMiner = 0;
-		CAmount aBud = 0;
-		CAmount aMnode = 0;
-		CAmount aFounders = 0;
-		CAmount aBlocks = 0;
+		coin += (small/COIN);
+		small = small%COIN;
+	}
+}
+
+void YearsReward(int iyears, int isum)
+{
+	CAmount aSumMr = 0, asmrsmall = 0;
+	CAmount aSumBd = 0, asbdsmall = 0;
+	CAmount aSumMn = 0, asmnsmall = 0;
+	CAmount aSumFr = 0, asfrsmall = 0;
+	CAmount aSumbk = 0, asbksmall = 0;
+	int lasth = 0;
+	cout << endl << "The rewards in every "<< iyears << " from 1st to 4th :" <<endl
+		<< "year         height range              MinerSubsidy           Budget        MasternodePayment      FoundersReward       BlockSubsidy" <<endl;
+	for(int i = 1; i < isum+1; i++)
+	{
+		CAmount aMiner = 0, aMinersmall = 0;
+		CAmount aBud = 0, aBudsmall = 0;
+		CAmount aMnode = 0, aMnodesmall = 0;
+		CAmount aFounders = 0, aFoundersmall = 0;
+		CAmount aBlocks = 0, aBlockssmall = 0;
 		int iHl = (i - 1) * 210240 * iyears;
 		int iHh = i * 210240 * iyears;
 		for (int h = iHl; h < iHh; h ++)
 		{
-			aMiner += GetMinerSubsidy(h, Params().GetConsensus());
-			aBud += GetBudget(h, Params().GetConsensus());
-			aMnode += GetMasternodePayment(h);
-			aFounders += GetFoundersReward(h, Params().GetConsensus());
-			aBlocks += (aMiner + aBud + aMnode + aFounders);
+			AddReward(GetMinerSubsidy(h, Params().GetConsensus()), aMiner, aMinersmall);
+			AddReward(GetMasternodePayment(h), aMnode, aMnodesmall);
+			if(CSuperblockManager::IsSuperblockTriggered(h))
+			{
+				AddReward(GetFoundersReward(h, Params().GetConsensus()), aFounders, aFoundersmall);
+				//if(CSuperblockManager::IsSuperblockVoteTriggered(h))
+				AddReward(GetBudget(h, Params().GetConsensus()), aBud, aBudsmall);
+			}
 		}
-		cout << i << " x" << iyears <<setw(9) << iHl << "--" << (iHh - 1)
+		aBlocks += (aMiner + aBud + aMnode + aFounders);
+        AddReward((aMinersmall + aBudsmall + aMnodesmall + aFoundersmall), aBlocks, aBlockssmall);
+		aSumMr += aMiner;
+		aSumBd += aBud;
+		aSumMn += aMnode;
+		aSumFr += aFounders;
+		asmrsmall += aMinersmall;
+		asbdsmall += aBudsmall;
+		asmnsmall += aMnodesmall;
+		asfrsmall += aFoundersmall;
+		lasth = iHh - 1;
+		cout << i << " x" << iyears << "     " << setw(9) << iHl << "--" << setw(9)  << (iHh - 1)
 			<< setw(20) << aMiner << setw(20) << aBud
 			<< setw(20) << aMnode << setw(20) << aFounders
 			<< setw(20) << aBlocks << endl;
 	}
+	aSumbk = (aSumMr + aSumBd + aSumMn + aSumFr);
+    AddReward((asmrsmall + asbdsmall + asmnsmall + asfrsmall), aSumbk, asbksmall);
+	cout << "total    " << setw(9) << 0 << "--" << setw(9)  << lasth                                                                                                                                                                             
+        << setw(20) << aSumMr << setw(20) << aSumBd
+        << setw(20) << aSumMn << setw(20) << aSumFr
+        << setw(20) << aSumbk << endl;
 }
 
 void RewardHandle()
 {
 	if (mapArgs.count("-height"))
 	{
-		cout << "  height      MinerSubsidy           Budget        MasternodePayment     FoundersReward       BlockSubsidy" << endl;
+		cout << "    height      MinerSubsidy           Budget        MasternodePayment     FoundersReward       BlockSubsidy" << endl;
 		for(string str : mapMultiArgs["-height"])
 		{
 			int h = atoi(str);
@@ -50,8 +88,8 @@ void RewardHandle()
 		}
 	}
 	
-	YearsReward(1);
-	YearsReward(4);
+	YearsReward(1, 4);
+	YearsReward(4, 5);
 }
 
 int main(int argc, char* argv[])
