90ee2882dad94a4b1ab8678ab6dc4d2a08661ba8
bicliu <klins@163.com>
2018-05-18 18:28:24 +0800

    update genesis

--- common/genesis.cpp
+++ common/genesis.cpp
@@ -30,6 +30,8 @@
 
 #include <sys/time.h>
 
+using namespace std;
+
 typedef uint32_t uint;
 typedef CBlockHeader ch;
 typedef long long ll;
@@ -110,6 +112,47 @@ static void findGenesis(CBlockHeader *pb, const std::string &net)
     }
 }
 
+static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint256 nNonce, uint32_t nBits, int32_t nVersion, const CAmount &genesisReward)
+{
+    CMutableTransaction txNew;
+    txNew.nVersion = 1;
+    txNew.vin.resize(1);
+    txNew.vout.resize(1);
+    txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+    txNew.vout[0].nValue = CAmount(genesisReward);
+    txNew.vout[0].scriptPubKey = genesisOutputScript;
+
+    CBlock genesis;
+    genesis.nTime    = nTime;
+    genesis.nBits    = nBits;
+    genesis.nNonce   = nNonce;
+    genesis.nVersion = nVersion;
+    genesis.vtx.push_back(txNew);
+    genesis.hashPrevBlock.SetNull();
+    genesis.hashMerkleRoot = BlockMerkleRoot(genesis);
+    genesis.hashClaimTrie = uint256S("0x1");
+    return genesis;
+}
+
+/**
+ * Build the genesis block. Note that the output of its generation
+ * transaction cannot be spent since it did not originally exist in the
+ * database.
+ *
+ * CBlock(hash=00000ffd590b14, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=e0028e, nTime=1390095618, nBits=1e0ffff0, nNonce=28917698, vtx=1)
+ *   CTransaction(hash=e0028e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
+ *     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d01044c5957697265642030392f4a616e2f3230313420546865204772616e64204578706572696d656e7420476f6573204c6976653a204f76657273746f636b2e636f6d204973204e6f7720416363657074696e6720426974636f696e73)
+ *     CTxOut(nValue=50.00000000, scriptPubKey=0xA9037BAC7050C479B121CF)
+ *   vMerkleTree: e0028e
+ */
+
+static CBlock CreateGenesisBlock1(uint32_t nTime, uint256 nNonce, uint32_t nBits, int32_t nVersion, const int64_t& genesisReward)                                                                                                                
+{
+    const char* pszTimestamp = "abracadabra";
+    const CScript genesisOutputScript = CScript() << ParseHex("041c508f27e982c369486c0f1a42779208b3f5dc96c21a2af6004cb18d1529f42182425db1e1632dc6e73ff687592e148569022cee52b4b4eb10e8bb11bd927ec0") << OP_CHECKSIG;
+    return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
+}
+
 void GenesisTest()
 {
     arith_uint256 nTempBit =  UintToArith256( Params().GetConsensus().powLimit);
@@ -119,5 +162,5 @@ void GenesisTest()
     cout << "\tpow:\t" << a.GetCompact()  << " "<< nTempBit.GetCompact() << endl;
     findGenesis(&genesis, "main");
     cout <<"hashMerkleRoot: " << genesis.hashMerkleRoot.ToString() << endl
-        << "hashGenesisBlock: " << genesis.GetHash() << endl;
+        << "hashGenesisBlock: " << genesis.GetHash().ToString() << endl;
 }
